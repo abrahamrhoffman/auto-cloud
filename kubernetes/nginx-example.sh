090066f7be9a07ff63a5f28ed9881efb5822f7c9
abe <abe@whitestar.hdyn.int>
2018-02-13 01:37:02 -0700

    minio

---
+++ kubernetes/nginx-example.sh
@@ -0,0 +1,1 @@
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.3.1/manifests/tutorial-2.yaml
--- kubernetes/pod-network.sh
+++ kubernetes/pod-network.sh
@@ -1,1 +1,1 @@
-kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml
